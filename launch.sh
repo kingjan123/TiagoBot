5fa52bc9264028c2b2123c8b617a115dd359aa62
javad maleky <mr.kingjan123@gmail.com>
2016-04-13 23:00:49 +0430

    Added files via upload

---
+++ launch.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+update() {
+  git pull
+  git submodule update --init --recursive
+  install_rocks
+}
+
+# Will install luarocks on THIS_DIR/.luarocks
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.2.1 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+  ./.luarocks/bin/luarocks install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lbase64 20120807-3
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+
+  ./.luarocks/bin/luarocks install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lua-cjson
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install serpent
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+}
+
+install() {
+  git pull
+  git submodule update --init --recursive
+  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  RET=$?;
+
+  cd tg
+  if [ $RET -ne 0 ]; then
+    autoconf -i
+  fi
+  ./configure && make
+
+  RET=$?; if [ $RET -ne 0 ]; then
+    echo "Error. Exiting."; exit $RET;
+  fi
+  cd ..
+  install_luarocks
+  install_rocks
+}
+
+if [ "$1" = "install" ]; then
+  install
+elif [ "$1" = "update" ]; then
+  update
+else
+  if [ ! -f ./tg/telegram.h ]; then
+    echo "tg not found"
+    echo "Run $0 install"
+    exit 1
+  fi
+
+  if [ ! -f ./tg/bin/telegram-cli ]; then
+    echo "tg binary not found"
+    echo "Run $0 install"
+    exit 1
+  fi
+  
+  #Adding some color. By @iicc1 :D
+   echo -e "\033[38;5;208m"
+   echo -e "      ____  ____ _____                        "
+   echo -e "     |    \|  _ )_   _|___ ____   __  __      "
+   echo -e "     | |_  )  _ \ | |/ .__|  _ \_|  \/  |     "
+   echo -e "     |____/|____/ |_|\____/\_____|_/\/\_|     "
+   echo -e "                                              \033[0;00m"
+   echo -e "\e[36m"
+
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+fi
